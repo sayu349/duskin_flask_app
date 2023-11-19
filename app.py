8e0b86cdb44d119c52a39dd8b73c34509de39fea
black76-yy <138761853+black76-yy@users.noreply.github.com>
2023-11-19 22:03:26 +0900

    Add files via upload
    製作途中のapp.pyです。classも途中までしか作れておらずDBが完成してないです。
    とりあえずモデル宣言のコードを完成させて、その後DBエンジンのコードを作る予定です。

--- app.py
+++ app.py
@@ -1,30 +1,70 @@
-from flask import Flask, render_template
+from sqlalchemy import create_engine,Column, Integer, String, or_
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
-# ==================================================
-# インスタンス生成
-# ==================================================
-app = Flask(__name__)
+class base(declarative_base):
+    pass
+#DB---Contract
+class Contract:
+    __tablename__ = "contracts"
 
-# ==================================================
-# ルーティング
-# ==================================================
-# TOPページ
-@app.route('/')
-def index():
-    return render_template('top.html')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    period_id: Mapped[int] = mapped_column(ForeignKey("period.id"))
+    customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"))
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+    contract_number: Mapped[int]
+    contract_situationm: Mapped[str] = mapped_column(str(30))
 
-# 一覧
-@app.route('/list')
-def item_list():
-    return render_template('list.html')
+    period : Mapped[list["Period"]] = relationship(
+        back_populates="contract", cascade="all, delete-orphan"
+    )
 
-# 詳細
-@app.route('/detail')
-def item_detail():
-    return render_template('detail.html')
+    customer : Mapped[list["Customer"]] = relationship(
+        back_populates="contract", cascade="all, delete-orphan"
+    )
 
-# ==================================================
-# 実行
-# ==================================================
-if __name__ == '__main__':
-    app.run()
+
+class Period:
+    __tablename__ = "period"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    week : Mapped[str] = mapped_column(ForeignKey("weeks.week"))
+    week_days : Mapped[str] = mapped_column(ForeignKey("week_days.week_day"))
+
+    contract: Mapped["Contract"] = relationship(back_populates="period")
+    week : Mapped[list["Week"]] = relationship(
+        back_populates="period", cascade="all, delete-orphan"
+    )
+    week_days : Mapped[list["Week_day"]] = relationship(
+        back_populates="period", cascade="all, delete-orphan"
+    )
+
+class Week:
+    __tablemame__ = "weeks"
+
+    week: Mapped[str] = mapped_column(primary_key=True)
+
+    period : Mapped[list["Period"]] = relationship(back_populates="weeks")
+
+class Week_day:
+    __tablename__ = "week_days"
+
+    week_day: Mapped[str] = mapped_column(primary_key=True)
+
+    period : Mapped[list["Period"]] = relationship(back_populates="week_days")
+
+class Customer:
+    __tablename__ = "customers"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    customer_name: Mapped[str] = mapped_column(str(15))
+    telephon_number: Mapped[int] = mapped_column(int(12))
+
+    contract : Mapped[Contract] = relationship(back_populates="customer")
+
