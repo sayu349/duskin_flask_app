8e9ac1d6dae9cf5abc7a938c967cf8aab2b7fb27
black76-yy <138761853+black76-yy@users.noreply.github.com>
2023-11-26 04:39:15 +0900

    更新しました。
    しばらく悩んでいたエラーが解決したのでアップします。
    クラス定義でのエラーだったためDB関連のファイルも新しくアップします。
    ただ新たに別のエラーが出たので引き続き頑張って直します。

--- app.py
+++ app.py
@@ -39,7 +39,13 @@ class Contract(db.Model):
     contract_number = db.Column(db.Integer)
     contract_situationm = db.Column(db.String)
 
+    #主リレーション
     pay = db.relationship("Pay", back_populates = "contracts", lazy=True)
+
+    #外部リレーション
+    period = db.relationship("Period", back_populates = "contracts")
+    customers = db.relationship("Customer", back_populates = "contracts")
+    products = db.relationship("Product", back_populates = "contracts")
 
     def __str__(self):
         return f'契約ID:{self.id}周期ID:{self.period_id}顧客ID{self.customer_id}商品ID:{self.product_id}契約数:{self.contract_number}契約状況:{self.contract_situationm}'
@@ -51,9 +57,14 @@ class Period(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     week = db.Column(db.String, db.ForeignKey("weeks.week"))
-    week_days = db.Column(db.String,db.ForeignKey("week_days.week_day"))
+    week_day = db.Column(db.String,db.ForeignKey("week_days.week_day"))
 
-    contracts = db.relationship("Contract",back_populates="period", lazy=True)
+    #主リレーション
+    contracts = db.relationship("Contract", back_populates="period", lazy=True)
+
+    #外部リレーション
+    weeks = db.relationship("Week", back_populates = "period")
+    week_days = db.relationship("Week_day", back_populates = "week_days")
   
     def __str__(self):
         return f'周期ID:{self.id}週:{self.weeks}曜日:{self.week_days}'
@@ -65,6 +76,7 @@ class Week(db.Model):
 
     week= db.Column(db.String, primary_key=True)
 
+    #主リレーション
     period = db.relationship("Period",back_populates="weeks", lazy=True)
 
     def __str__(self):
@@ -77,6 +89,7 @@ class Week_day(db.Model):
     
     week_day= db.Column(db.String, primary_key=True)
 
+    #主リレーション
     period = db.relationship("Period",back_populates="week_days", lazy=True)
 
     def __str__(self):
@@ -91,6 +104,7 @@ class Customer(db.Model):
     customer_name = db.Column(db.String)
     telephon_number = db.Column(db.Integer)
 
+    #主リレーション
     contracts = db.relationship("Contract", back_populates="customer", lazy=True)
     pay = db.relationship("Pay", back_populates = "customers", lazy=True)
 
@@ -106,7 +120,8 @@ class Product(db.Model):
     product_name = db.Column(db.String)
     product_price = db.Column(db.Integer)
 
-    contracts = db.relationship("Conract", back_populates="product", lazy=True)
+    #主リレーション
+    contracts = db.relationship("contracts", back_populates="product", lazy=True)
 
     def __str__(self):
         return f'商品ID:{id}商品名:{self.product_name}商品価格:{self.product_price}'
@@ -119,32 +134,44 @@ class Pay(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))#この行はいらないかも？
     contract_id  = db.Column(db.Integer, db.ForeignKey("contracts.id"))
-    pay_methood_id  = db.Column(db.Integer, db.ForeignKey("pay_methood.id"))
+    pay_method_id  = db.Column(db.Integer, db.ForeignKey("pay_method.id"))
     #pay_total消した
+    #外部リレーション
+    customers = db.relationship("Customer", back_populates = "pay")
+    contracts = db.relationship("Contract", back_populates = "pay")
+    pay_method = db.relationship("Pay_method", back_populates = "pay")
+
 
     def __str__(self):
-        return f'支払ID:{self.id}顧客ID:{self.customer_id}契約ID:{self.contract_id}支払方法ID:{self.pay_methood_id}'
+        return f'支払ID:{self.id}顧客ID:{self.customer_id}契約ID:{self.contract_id}支払方法ID:{self.pay_method_id}'
 
 
-class Pay_methood(db.Model):
+class Pay_method(db.Model):
     
-    __tablename__ = "pay_methood"
+    __tablename__ = "pay_method"
 
     id = db.Column(db.Integer, primary_key=True)
-    pay_methood_name = db.Column(db.String)
+    pay_method_name = db.Column(db.String)
 
-    pay = db.relationship("Pay",back_populates="pay_methood", lazy=True)
+    #外部リレーション
+    pay = db.relationship("Pay",back_populates="pay_method", lazy=True)
 
     def __str__(self):
-        return f'支払方法ID:{self.id}支払方法:{self.pay_methood_name}'
+        return f'支払方法ID:{self.id}支払方法:{self.pay_method_name}'
 
 
 @app.route("/")
 def index():
         
-    money_customer_total = select(Contract.contract_number).select(Product.product_price).select(func.count(distinct(Contract.customer_id))).join(Product).where(Contract.contract_situationm != "解約済み")
-    
-    Period_lists = Period.query.all()
+    money_customer_total = (
+        db.session.query(db.func.count(distinct(Contract.customer_id)).label("customer_total") , db.func.sum(Contract.contract_number * Product.product_price).label("money_total"))
+        .filter(Contract.product_id == Product.id)
+    )
+
+
+    Period_lists = (
+        db.session.query(Period)
+    )
 
     return render_template("index.html", money_customer_total=money_customer_total, Period_lists=Period_lists)
 
@@ -172,12 +199,12 @@ def create_contract():
 
 @app.route("/period_contract/<int:period_id>", methods = ["POST"])
 def list(period_id):
-    period_by_contract_list =select(Contract).where(Contract.contract_situationm != "解約済み").where(Contract.period_id == period_id).order_by(Contract.customer_id)
+    period_by_contract_list =db.select(Contract).where(Contract.contract_situationm != "解約済み").where(Contract.period_id == period_id).order_by(Contract.customer_id)
         
-    customer_by_contract_cutomer_total = select(sum(Product.product_price * Contract.contract_number)).select(Customer).join(Product).join(Customer).where(Contract.contract_situationm != "解約済み").where(Contract.period_id == period_id).group_by(Contract.customer_id)
+    customer_by_contract_cutomer_total = db.select(sum(Product.product_price * Contract.contract_number)).db.select(Customer).join(Product).join(Customer).where(Contract.contract_situationm != "解約済み").where(Contract.period_id == period_id).group_by(Contract.customer_id)
     
         
-    period_by_contract_cutomer_total = select(sum(Product.product_price * Contract.contract_number)), func.count(distinct(Contract.customer_id)).join(Product).join(Customer).where(Contract.contract_situationm != "解約済み").where(Contract.period_id == period_id)
+    period_by_contract_cutomer_total = db.select(sum(Product.product_price * Contract.contract_number)), func.count(distinct(Contract.customer_id)).join(Product).join(Customer).where(Contract.contract_situationm != "解約済み").where(Contract.period_id == period_id)
 
 
     return render_template("period_contract_list.html", period_by_contract_list=period_by_contract_list ,
@@ -187,14 +214,14 @@ def list(period_id):
 
 @app.route("/product")
 def product():
-    prouduct_lists =select(Product).order_by(Product.id)
+    prouduct_lists =db.select(Product).order_by(Product.id)
 
     return render_template("product.html", prouduct_lists=prouduct_lists)
 
 
 @app.route("/product/<int:product_id>", methods = ["POST"])
 def product_by_contract(product_id):
-    product_by_contract_lists = select(Contract.period_id,Customer.customer_name,Customer.telephon_number) .join(Customer).where(Contract.product_id == product_id).where(Contract.contract_situationm != "解約済み").order_by(Contract.customer_id)
+    product_by_contract_lists = db.select(Contract.period_id,Customer.customer_name,Customer.telephon_number) .join(Customer).where(Contract.product_id == product_id).where(Contract.contract_situationm != "解約済み").order_by(Contract.customer_id)
     
     return render_template("product_by_contract.html", product_by_contract_lists=product_by_contract_lists)
 
