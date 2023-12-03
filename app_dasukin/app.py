94e835acb669505244c8bd7b241e6ff8dcfb8969
black76-yy <138761853+black76-yy@users.noreply.github.com>
2023-12-03 21:02:20 +0900

    更新しました。
    大きな変更点としては、
    １，ルーティングにある関数でのクエリ文を変更しました。
    ２，データ入力をしていて使わないかも？と感じたものを削除してみました。（削除前のクラスが書いてあるfileは僕が別ディレクトリでバックアップとってあるのですぐ復元できます。）
    ３，data_add.pyを新しく作成しました。このファイルを実行するとdb.sqliteに仮データが入ります。

---
+++ app_dasukin/app.py
@@ -0,0 +1,161 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
+import pandas as pd
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy import select, distinct, func, join
+
+app = Flask(__name__)
+
+#DBファイル設定
+app.config['SECRET_KEY'] = os.urandom(24)
+base_dir = os.path.dirname(__file__)
+database = "sqlite:///" + os.path.join(base_dir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = database
+app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#基本クラス作成
+class Base(DeclarativeBase):
+    pass
+
+
+
+Migrate(app, db)
+#モデル宣言
+#サブクラス作成
+class Contract(db.Model):
+    
+    __tablename__ = "contracts"
+
+    contract_id = db.Column(db.Integer, primary_key=True)
+    period_id = db.Column(db.String, db.ForeignKey("period.period_id"))
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.customer_id"))
+    product_id = db.Column(db.String, db.ForeignKey("products.product_id"))
+    contract_number = db.Column(db.Integer)
+    contract_situation = db.Column(db.String)
+    pay_method_id = db.Column(db.String)
+
+
+
+    def __str__(self):
+        return f'契約ID:{self.contract_id}周期ID:{self.period_id}顧客ID{self.customer_id}商品ID:{self.product_id}契約数:{self.contract_number}契約状況:{self.contract_situation}支払方法{self.pay_method_id}'
+
+
+class Period(db.Model):
+    
+    __tablename__ = "period"
+
+    period_id = db.Column(db.String, primary_key=True)
+
+    #主リレーション
+    contracts = db.relationship("Contract", backref = "period")
+
+  
+    def __str__(self):
+        return f'周期ID:{self.period_id}'
+
+
+
+class Customer(db.Model):
+    
+    __tablename__ = "customers"
+
+    customer_id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String)
+    telephon_number = db.Column(db.Integer)
+
+    #主リレーション
+    contracts = db.relationship("Contract", backref="customers")
+
+    def __str__(self):
+        return f'顧客ID:{self.customer_id}顧客名:{self.customer_name}電話番号:{self.telephon_number}'
+
+
+class Product(db.Model):
+    
+    __tablename__ = "products"
+
+    product_id = db.Column(db.String, primary_key=True)
+    product_name = db.Column(db.String)
+    product_price = db.Column(db.Integer)
+
+    #主リレーション
+    contracts = db.relationship("Contract", backref="products")
+
+    def __str__(self):
+        return f'商品ID:{self.product_id}商品名:{self.product_name}商品価格:{self.product_price}'
+
+
+@app.route("/")
+def index():
+    customer_total = Customer.query.count()
+
+    money_total = Contract.query.join(Product).with_entities(Contract.contract_number * Product.product_price).filter(Contract.contract_situation == "契約済")    
+    Period_lists = Period.query.all()
+
+    return render_template("index.html", customer_total=customer_total,money_total=money_total ,Period_lists=Period_lists)
+
+
+@app.route("/create_contract", methods = ["GET","POST"])
+def create_contract():
+    # POST
+    if request.method == 'POST':
+        # 入力値取得
+        contract_id = request.form.get('contract_id')
+        period_id = request.form.get('period_id')
+        customer_id = request.form.get('customer_id')
+        product_id = request.form.get('product_id')
+        contract_number = request.form.get('contract_number')
+        contract_situation = request.form.get('contract_situation')
+        # インスタンス生成
+        contract = Contract(contract_id=contract_id, period_id=period_id, customer_id=customer_id, product_id=product_id, contract_number=contract_number, contract_situation=contract_situation)
+        # 登録
+        db.session.add(contract)
+        db.session.commit()
+        # 一覧へ
+        return redirect("/")
+    # GET
+    return render_template('create_contract.html')
+
+
+@app.route("/period_contract/<period_id>")
+def list(period_id):
+    period_by_contract_list = Contract.query.join(Customer,Product).filter(Contract.contract_situation != "解約済み", Contract.period_id == period_id).all()
+    
+    groupby_customer_money = Contract.query.join(Product).group_by(Contract.customer_id).with_entities(Contract.contract_number * Product.product_price,Customer.customer_name,Customer.telephon_number).filter(Contract.contract_situation == "契約済", Contract.period_id == period_id)
+    
+
+    period_customer_total = Contract.query.filter(Contract.contract_situation == "契約済", Contract.period_id == period_id).group_by(Contract.customer_id).count()
+
+    period_money_total = Contract.query.group_by(Contract.customer_id).filter(Contract.contract_situation != "解約済み", Contract.period_id == period_id)
+
+    return render_template("period_contract_list.html", period_by_contract_list=period_by_contract_list
+                           ,groupby_customer_money=groupby_customer_money,period_customer_total=period_customer_total
+                           ,period_money_total=period_money_total)
+
+
+@app.route("/product")
+def product():
+    prouduct_lists =db.session.query(db.select(Product.product_id,Product.product_name, Product.product_price)
+                    .order_by(Product.product_id)
+                    )
+
+    return render_template("product.html", prouduct_lists=prouduct_lists)
+
+
+@app.route("/product/<product_id>", methods = ["POST"])
+def product_by_contract(product_id):
+    product_by_contract_lists = (
+        db.session.query(Contract.period_id, Customer.customer_name, Customer.telephon_number)
+        .filter(Contract.contract_situationm != "解約済み", Contract.product_id == product_id)
+        .order_by(Contract.customer_id)
+    )
+    return render_template("product_by_contract.html", product_by_contract_lists=product_by_contract_lists)
+
+if __name__ == '__main__':
+    app.run()
---
+++ app_dasukin/data_add.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app import db, Period, app, Product, Customer
+
+
+
+with app.app_context():
+
+#Periodデフォルト登録
+    A1 = Period(period_id = "A月")
+    A2 = Period(period_id = "A火")
+    A3 = Period(period_id = "A水")
+    A4 = Period(period_id = "A木")
+    A5 = Period(period_id = "A金")
+    A6 = Period(period_id = "A土")
+    A7 = Period(period_id = "A日")
+
+    B1 = Period(period_id = "B月")
+    B2 = Period(period_id = "B火")
+    B3 = Period(period_id = "B水")
+    B4 = Period(period_id = "B木")
+    B5 = Period(period_id = "B金")
+    B6 = Period(period_id = "B土")
+    B7 = Period(period_id = "B日")
+
+    C1 = Period(period_id = "C月")
+    C2 = Period(period_id = "C火")
+    C3 = Period(period_id = "C水")
+    C4 = Period(period_id = "C木")
+    C5 = Period(period_id = "C金")
+    C6 = Period(period_id = "C土")
+    C7 = Period(period_id = "C日")
+
+    D1 = Period(period_id = "D月")
+    D2 = Period(period_id = "D火")
+    D3 = Period(period_id = "D水")
+    D4 = Period(period_id = "D木")
+    D5 = Period(period_id = "D金")
+    D6 = Period(period_id = "D土")
+    D7 = Period(period_id = "D日")
+
+    db.session.add_all([A1,A2,A3,A4,A5,A6,A7,B1,B2,B3,B4,B5,B6,B7,C1,C2,C3,C4,C5,C6,C7,D1,D2,D3,D4,D5,D6,D7])
+    db.session.commit()
+#↑↑↑↑↑
+
+#Productデフォルト登録
+with app.app_context():
+    mop = Product(product_id = "S212", product_name = "ゴールデンモップ", product_price = 930)
+
+    db.session.add_all([mop])
+    db.session.commit()
+
+
+
+with app.app_context():
+    user1 = Customer(customer_id = "1", customer_name = "Aさん", telephon_number = 2221111333)
+
+    db.session.add_all([user1])
+    db.session.commit()
